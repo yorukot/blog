c0cbada5d2f73dd34f6e7a6add395549b1cb3ebf
Yorukot <hi@yorukot.me>
2025-06-27 03:06:32 +0800

    feat: enhance site configuration and update components with new features

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,20 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 import icon from "astro-icon";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://yorukot.me", // Replace with your actual domain
   vite: {
     plugins: [tailwindcss()],
   },
-  integrations: [icon()],
+  integrations: [
+    icon(),
+    sitemap({
+      xslURL: "/sitemap.xsl",
+    }),
+  ],
 });
